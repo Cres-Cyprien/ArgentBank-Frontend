1f9181c33daa08333de5eb4464239771ab94ebf4
Cyprien <cres.cyprien@gmail.com>
2024-01-31 23:26:57 +0100

    Test

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "../pages/home/Home"
 import Login from "../features/auth/login/Login"
 import User from "../features/user/User"
